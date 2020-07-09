53680799c594eb1005341e817826a2f3b01b8e79
M Rajesh <m.rajeshrajz@gmail.com>
2020-07-09 11:00:13 +0530

    added initial app

---
+++ src/test/java/com/lwl/testmanagement/TestManagementServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lwl.testmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestManagementServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
